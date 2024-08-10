5861dba1444a727bc8992ac7b2431a74e12910d6
Gaurav Kumar <gauravgupta0216@gmail.com>
2024-08-11 00:48:22 +0530

    knapsack Memoization

--- DynamicProgramming/Knapsack.playground/Contents.swift
+++ DynamicProgramming/Knapsack.playground/Contents.swift
@@ -4,4 +4,11 @@ let weight: [Int] = [1, 3, 4, 5]
 let value: [Int] = [1, 4, 5, 7]
 let capacity: Int = 7
 
-let maxProfit = knapsackRecursive(weight: weight, value: value, capacity: capacity, size: weight.count)
+//Knapsack via Recursion
+KnapsackRecursive().calculate(weight: weight, value: value, capacity: capacity, size: weight.count)
+
+
+//knapsack via Memoization
+var dpArray: [[Int]] = Array(repeating: [Int](repeating: -1, count: capacity+1), count: weight.count+1)
+var obj = KnapsackMemoization(dp: dpArray)
+obj.calculate(weight: weight, value: value, capacity: capacity, size: weight.count)
--- DynamicProgramming/Knapsack.playground/Sources/KnapsackRecursive.swift
+++ DynamicProgramming/Knapsack.playground/Sources/KnapsackRecursive.swift
@@ -1,15 +1,18 @@
 import Foundation
 
-public func knapsackRecursive(weight: [Int], value: [Int], capacity: Int, size: Int) -> Int {
-    if capacity == 0 || size == 0 {
-        return 0
-    }
-    if weight[size - 1] <= capacity {
-        return max(
-            value[size - 1] + knapsackRecursive(weight: weight, value: value, capacity: capacity - weight[size - 1], size: size - 1),
-            knapsackRecursive(weight: weight, value: value, capacity: capacity, size: size - 1)
-        )
-    } else {
-        return knapsackRecursive(weight: weight, value: value, capacity: capacity, size: size - 1)
+public struct KnapsackRecursive {
+    public init() {}
+    public func calculate(weight: [Int], value: [Int], capacity: Int, size: Int) -> Int {
+        if capacity == 0 || size == 0 {
+            return 0
+        }
+        if weight[size - 1] <= capacity {
+            return max(
+                value[size - 1] + calculate(weight: weight, value: value, capacity: capacity - weight[size - 1], size: size - 1),
+                calculate(weight: weight, value: value, capacity: capacity, size: size - 1)
+            )
+        } else {
+            return calculate(weight: weight, value: value, capacity: capacity, size: size - 1)
+        }
     }
 }
