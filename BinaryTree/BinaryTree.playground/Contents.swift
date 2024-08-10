7302dd5d2f9b37289844745982bdb177e2f7fd30
Gaurav Kumar <gauravgupta0216@gmail.com>
2024-08-10 22:44:54 +0530

    Code Refactor

---
+++ BinaryTree/BinaryTree.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let tree = BST(value: 4)
+
+// Insert elements
+tree.insert(2)
+tree.insert(1)
+tree.insert(3)
+tree.insert(5)
+
+// Check search
+print(tree.search(4)) // Should be true
+print(tree.search(6)) // Should be false
