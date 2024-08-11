06b5f219e19e52e1a0c928b119a61b1aa41d3f9b
Gaurav Kumar <gauravgupta0216@gmail.com>
2024-08-12 00:22:47 +0530

    folder restructure

--- Recursion.playground/Sources/Fibonacci.swift
+++ Recursion.playground/Sources/Fibonacci.swift
@@ -1,7 +1,9 @@
 import Foundation
 
+import Foundation
+
 public func getFib(_ position: Int) -> Int {
-    if position == 0 || position == 1 {
+    if position <= 1 {
         return position
     }
     return getFib(position - 1) + getFib(position - 2)
